ac2fbc0963fc372fc1630ed812b33977c1b9a4ac
mohrsignal <mohrsignal@gmail.com>
2018-07-23 21:03:47 -0700

    Add barchart() and dotplot()

--- R/visualize.R
+++ R/visualize.R
@@ -19,7 +19,7 @@ compare_histogram <- function(df, x,
          subtitle = subtitle) +
     theme_minimal() +
     theme(strip.text = element_text(size = 14))
-
+  
   if (log_scale) {
     
     g <- g + scale_x_log10()
@@ -36,24 +36,71 @@ compare_histogram <- function(df, x,
 barchart <- function(df, x, y,
                      title = NULL,
                      subtitle = NULL,
-                     caption = NULL) {
-
+                     caption = NULL,
+                     xlab = NULL,
+                     ylab = NULL) {
+  
   x <- enquo(x)
   y <- enquo(y)
-
+  
   ggplot(df) + 
-    geom_col(aes(x = reorder(!!x, !!y), y = !!y)) +
+    geom_col(aes(x = reorder(!!x, !!y), y = !!y),
+             fill = rgb(136, 189, 230, max = 255)) +
     coord_flip() +
     labs(title = title,
          subtitle = subtitle,
-         caption = caption) +
-    theme_minimal()
+         caption = caption,
+         x = xlab,
+         y = ylab) +
+    theme_minimal() +
+    scale_x_discrete(expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    theme(panel.background = element_blank(),
+          axis.line = element_line(color = rgb(140, 140, 140, max = 255)),
+          strip.text = element_blank(),
+          axis.title.x = element_text(size = 12),
+          axis.title.y = element_text(size = 11, angle = 0),
+          axis.text = element_text(size = 11),
+          axis.ticks = element_blank())
+}
+
+dotplot <- function(df, x, y,
+                    title = NULL,
+                    subtitle = NULL,
+                    caption = NULL,
+                    xlab = NULL,
+                    ylab = NULL) {
+  
+  x <- enquo(x)
+  y <- enquo(y)
+  
+  ggplot(df) + 
+    geom_point(aes(x = reorder(!!x, !!y), y = !!y),
+               color = rgb(93, 165, 218, max=255),
+               size = 2) +
+    coord_flip() +
+    labs(title = title,
+         subtitle = subtitle,
+         caption = caption,
+         x = xlab,
+         y = ylab) +
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
+    theme_minimal() +
+    theme(panel.background = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.grid.major.y = element_line(linetype = "dashed",
+                                            color = "grey80"), 
+          axis.line = element_line(color = rgb(140, 140, 140, max = 255)),
+          strip.text = element_blank(),
+          axis.title.x = element_text(size = 12),
+          axis.title.y = element_text(size = 11, angle = 0),
+          axis.text = element_text(size = 11),
+          axis.ticks = element_blank())
 }
 
 network_graph <- function(network) {
   
   ggraph(network) + 
-    #geom_edge_link(aes(edge_colour = weight)) +
     geom_edge_link(aes(edge_width = weight), alpha = 0.5) +
     geom_node_point() +
     geom_node_text(aes(label = label), repel = TRUE) +
